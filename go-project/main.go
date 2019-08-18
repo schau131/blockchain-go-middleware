c271ee831c2432b5ba271db6768ac1b34c383408
shashank <shashank.24@outlook.com>
2019-08-18 22:26:29 +0530

    added URL for register user and add channel

--- go-project/main.go
+++ go-project/main.go
@@ -4,12 +4,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"io/ioutil"
 	"github.com/gorilla/mux"
+	"encoding/json"
+	"os"
+	"bytes"
+	"strings"
 )
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
+	r.HandleFunc("/users", registerUser).Methods("GET")
+	r.HandleFunc("/create-channel", createChannel).Methods("GET")
 	r.HandleFunc("/hello", handler).Methods("GET")
 	return r
 }
@@ -20,6 +27,102 @@ func main() {
 
 	http.ListenAndServe(":8080", r)
 
+}
+
+type RegisterRequest struct{
+	Username string `json:"username"`
+	Orgname string `json:"orgName"`
+}
+
+type RegisterResponse struct{
+	Success bool `json:"success"`
+	Secret string `json:"secret"`
+	Message string `json:"message"`
+	Token string `json:"token,omitempty"`
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	
+	fmt.Println(" Forwarding the request")
+	
+//	register := RegisterRequest{}
+//	register.Username = r.URL.Query().Get("username")
+//	register.Orgname = r.URL.Query().Get("orgName")
+	
+//	var jsonStr []byte
+//	jsonStr, err := json.Marshal(register)
+	
+//	fmt.Println(string(jsonStr))
+	
+	data := url.Values{}
+	data.Set("username", r.URL.Query().Get("username"))
+	data.Set("orgName", r.URL.Query().Get("orgName"))
+	
+	fmt.Println(strings.NewReader(data.Encode()))
+	
+	client := &http.Client{
+		//CheckRedirect: redirectPolicyFunc,
+	}
+	
+	req, err := http.NewRequest("POST", "http://localhost:4000/users", strings.NewReader(data.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	response, err := client.Do(req)
+	
+	
+	if err != nil {
+		fmt.Printf("The Http request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		
+		var resJson RegisterResponse
+		json.Unmarshal(data, &resJson)
+		
+		f, err := os.Create("token.txt")
+		
+		if err != nil {
+			fmt.Printf("The Http request failed with error %s\n", err)
+		}
+		
+		f.WriteString(resJson.Token)
+		f.Close()
+		
+		resJson.Token = ""
+		
+		resJsonStr, err := json.Marshal(resJson)
+		fmt.Fprintf(w, string(resJsonStr))
+	}
+}
+
+func createChannel(w http.ResponseWriter, r *http.Request) {
+	
+	fmt.Println("Creating Channel")
+	
+	jsonStr := []byte(`{"channelName":"mychannel","channelConfigPath":"../artifacts/channel/mychannel.tx"}`)
+		
+	client := &http.Client{
+		//CheckRedirect: redirectPolicyFunc,
+	}
+	
+	file, err := os.Open("token.txt")
+	defer file.Close()
+	b, _ := ioutil.ReadAll(file)
+	authToken := "Bearer " + string(b)
+
+	fmt.Println(authToken)
+	
+	req, err := http.NewRequest("POST", "http://localhost:4000/channels", bytes.NewReader(jsonStr))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("authorization", authToken)
+	response, err := client.Do(req)
+	
+	
+	if err != nil {
+		fmt.Printf("The Http request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+
+		fmt.Fprintf(w, string(data))
+	}
 }
 
 
